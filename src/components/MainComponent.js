506fe68586f11de8de6c860f70c08c3b49288e21
Aman <styloamnald@gmail.com>
2020-06-26 20:16:44 +0530

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,11 +5,12 @@ import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
 import { PROMOTIONS } from "../shared/promotions";
 import { LEADERS } from "../shared/leaders";
-import Dish from "./DishdetailComponent";
+import Dishdetail from "./DishdetailComponent";
 import Contact from "./ContactComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import About from "./AboutComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
@@ -36,7 +37,7 @@ class Main extends Component {
 
     const DishWithId = ({ match }) => {
       return (
-        <DishDetail
+        <Dishdetail
           dish={
             this.state.dishes.filter(
               (dish) => dish.id === parseInt(match.params.dishId, 10)
@@ -58,6 +59,11 @@ class Main extends Component {
             exact
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route
+            exact
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
           />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
